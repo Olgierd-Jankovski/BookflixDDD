78baaf0fb656fed897878240d56f92e34cd9b5f6
Olgierd-Jankovski <olgierdjankovski@gmail.com>
2024-03-17 14:02:52 +0200

    Add custom ProblemDetailsFactory, ErrorController and disable launch browser. Add exception handler to re-execute the request to error handling path

--- Bookflix.Api/Program.cs
+++ Bookflix.Api/Program.cs
@@ -1,15 +1,20 @@
+using Bookflix.Api.Errors;
 using Bookflix.Application;
 using Bookflix.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 {   
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>(); // Override ProblemDetailsFactory
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
