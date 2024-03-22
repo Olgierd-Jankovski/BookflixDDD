cf7ad62e2075cffaf0422442cf174836f73e8325
Olgierd-Jankovski <olgierdjankovski@gmail.com>
2024-03-22 16:28:44 +0200

    Fix typo in BookReviews table column definition

---
+++ Bookflix.Infrastructure/Migrations/20240322142432_TypoFix.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookflix.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TypoFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "BookReviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "BookReviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
