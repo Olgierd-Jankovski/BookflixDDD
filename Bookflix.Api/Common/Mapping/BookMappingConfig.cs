428004d4620b348adfa6b1291a973656341e79fb
Olgierd-Jankovski <olgierdjankovski@gmail.com>
2024-03-30 15:25:11 +0200

    Update mapping for BookResponse in BookMappingConfig

--- Bookflix.Api/Common/Mapping/BookMappingConfig.cs
+++ Bookflix.Api/Common/Mapping/BookMappingConfig.cs
@@ -13,6 +13,12 @@ public class BookMappingConfig : IRegister
         config.NewConfig<(CreateBookRequest Request, int AuthorId), CreateBookCommand>()
             .Map(dest => dest.AuthorId, src => src.AuthorId)
             .Map(dest => dest, src => src.Request);
-        
+
+        config.NewConfig<Book, BookResponse>()
+            .Map(dest => dest.UpdatedAt, src => src.DateTimeInfo.UpdatedDateTime)
+            .Map(dest => dest.CreatedAt, src => src.DateTimeInfo.CreatedDateTime)
+            .Map(dest => dest.Genres, src => src.Genres.Select(g => new GenreResponse(g.Id, g.Genre.ToString())).ToList())
+            .Map(dest => dest.Reviews, src => src.Reviews.Select(r => new BookReviewResponse(r.Id, r.Rating.Value, r.Comment, r.AuthorIdentityGuid, 123)).ToList())
+            .Map(dest => dest, src => src);
     }
 }
