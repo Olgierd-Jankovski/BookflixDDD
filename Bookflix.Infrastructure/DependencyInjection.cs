aaf2a7ae37ed901228e39a9706474f59240f28b2
Olgierd-Jankovski <olgierdjankovski@gmail.com>
2024-04-02 15:45:58 +0300

    Refactor dependency injection and authentication configuration

--- Bookflix.Infrastructure/DependencyInjection.cs
+++ Bookflix.Infrastructure/DependencyInjection.cs
@@ -21,14 +21,10 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
         ConfigurationManager configuration)
-    {   
+    {
         services.AddAuth(configuration).AddPersistence();
         services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
-        services.AddScoped<IIDentityService, IdentityService>();
-        services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IBookRepository, BookRepository>();
-        services.AddScoped<IAuthorRepository, AuthorRepository>();
 
         return services;
     }
@@ -39,35 +35,40 @@ public static class DependencyInjection
 
         services.AddDbContext<AppDbContext>(options =>
         {
-        options.UseSqlServer(
-            @"Server=(localdb)\mssqllocaldb;Database=BookflixDB;Trusted_Connection=True");
+            options.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=BookflixDB;Trusted_Connection=True");
         });
+
+        services.AddScoped<IIDentityService, IdentityService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IAuthorRepository, AuthorRepository>();
 
         return services;
     }
 
-public static IServiceCollection AddAuth(
- this IServiceCollection services,
-    ConfigurationManager configuration)
-{
-    var jwtSettings = new JwtSettings();
-    configuration.Bind(JwtSettings.SectionName, jwtSettings);
+    public static IServiceCollection AddAuth(
+     this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
 
-    services.AddSingleton(Options.Create(jwtSettings));
-    services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
 
-    services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
-        });
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            });
 
-    return services;
-}
+        return services;
+    }
 }
